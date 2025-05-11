ae6a5f3c1a4767bd0516bc94aee96fb59b3d05c0
Alex Z <email.xingzhou@gmail.com>
2025-05-11 00:19:36 -0700

    Create file

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,21 @@
+// Last updated: 5/11/2025, 12:19:36 AM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int p = 0;
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            if (nums[i] != 0) 
+            {
+                nums[p++] = nums[i];
+            }
+        }
+
+        while (p < nums.size()) 
+        {
+            nums[p++]= 0;
+        }
+
+        return;
+    }
+};
